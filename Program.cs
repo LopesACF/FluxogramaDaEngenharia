12a58acc9d065ceaaafdd46b8faf239c415dd3b5
Altemilson Lopes <altemilsonlopes@gmail.com>
2023-05-01 19:44:49 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---\n");
+
+Console.WriteLine("O objeto esta se movendo(S/N)?");
+string movendo = Console.ReadLine()!.Substring(0,1).ToUpper();
+
+
+if(movendo == "S"){
+Console.WriteLine("Deveria? S/N");
+string deveria = Console.ReadLine()!.Substring(0,1).ToUpper();
+if (deveria == "S"){
+    Console.WriteLine("Ótimo!");
+} 
+else if(deveria == "N"){
+    Console.WriteLine("Use SilverTape.");
+}
+else {
+    Console.WriteLine("Use apenas S/N");
+}
+}
+
+
+
+if (movendo == "N"){
+    Console.WriteLine("Deveria? S/N");
+    string deveria = Console.ReadLine()!.Substring(0,1).ToUpper();
+    if(deveria == "S"){
+        Console.WriteLine("Use  WD-40.");
+    }
+    else if (deveria == "N"){
+        Console.WriteLine("Ótimo!");
+    }
+    else {Console.WriteLine("Use apenas S/N");
+}
+}
+
+
+
+    
+  
